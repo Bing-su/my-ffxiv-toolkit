63681b4ccd850c5fbfe0112ac1d5cd80d945f16f
Bingsu <ks2515@naver.com>
2022-05-04 23:16:14 +0900

    feat: 완성

---
+++ main.py
@@ -0,0 +1,26 @@
+import argparse
+import json
+from pathlib import Path
+
+from scrapper import FFXIVDataScrapper
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-j", "--json", default="default.json", help="스크랩 정보를 담은 json파일의 경로"
+    )
+    parser.add_argument("-p", "--path", default="./data", help="결과 파일 저장 경로")
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    with open(args.json) as file:
+        info = json.load(file)
+
+    Path(args.path).mkdir(parents=True, exist_ok=True)
+
+    scrapper = FFXIVDataScrapper(info, args.path)
+    scrapper.scrap()
---
+++ scrapper.py
@@ -0,0 +1,50 @@
+from concurrent.futures import ThreadPoolExecutor
+from functools import partial
+from io import StringIO
+from typing import Literal, Optional
+
+import pandas as pd
+import requests
+
+
+class FFXIVDataScrapper:
+    KO_BASE_URL = (
+        "https://raw.githubusercontent.com/Ra-Workspace/ffxiv-datamining-ko/master/csv"
+    )
+    EN_BASE_URL = "https://raw.githubusercontent.com/xivapi/ffxiv-datamining/master/csv"
+
+    def __init__(self, info: dict[str, list[str]], save_dir: str = "./data") -> None:
+        self.info = info
+        self.names = tuple(self.info.keys())
+        self.save_dir = save_dir
+
+    def url_to_df(self, name: str, lang: Literal["en", "ko"]) -> pd.DataFrame:
+        base_url = self.KO_BASE_URL if lang == "ko" else self.EN_BASE_URL
+        url = f"{base_url}/{name}.csv"
+        columns = ["#"] + self.info[name]
+        resp = requests.get(url).text
+        data = StringIO(resp)
+        df = pd.read_csv(
+            data, usecols=columns, skiprows=1, index_col=0, low_memory=False
+        )
+        return df
+
+    def make_concat_df(self, name: str) -> pd.DataFrame:
+        dfs = [self.url_to_df(name, lang) for lang in ("en", "ko")]
+        cat_df = pd.concat(dfs, axis=1)
+        return cat_df
+
+    def scrap(self, save_dir: Optional[str] = None) -> None:
+        if save_dir is None:
+            save_dir = self.save_dir
+
+        save_func = partial(self.save, save_dir=save_dir)
+        with ThreadPoolExecutor() as executor:
+            dfs = tuple(executor.map(self.make_concat_df, self.names))
+            executor.map(save_func, dfs, self.names)
+
+    def save(self, df: pd.DataFrame, name: str, save_dir: Optional[str] = None) -> None:
+        if save_dir is None:
+            save_dir = self.save_dir
+        path = f"{save_dir}/{name}.xlsx"
+        df.to_excel(path)
