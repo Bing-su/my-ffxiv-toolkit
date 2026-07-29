fd1665552cc129ff91f3ae66867461e1ecb91ffb
Dowon <ks2515@naver.com>
2026-07-29 13:14:05 +0900

    fix: update tc repo url

--- src/bingkit/ffxiv/_base.py
+++ src/bingkit/ffxiv/_base.py
@@ -11,7 +11,7 @@ BASE_URL: dict[str, str] = {
     "ja": "https://raw.githubusercontent.com/skyborn-industries/xiv-data/refs/heads/main/exd/ja/{name}.csv",
     "cn": "https://raw.githubusercontent.com/thewakingsands/ffxiv-datamining-cn/refs/heads/master/{name}.csv",
     "ko": "https://raw.githubusercontent.com/Ra-Workspace/ffxiv-datamining-ko/refs/heads/master/csv/{name}.csv",
-    "tc": "https://raw.githubusercontent.com/skyborn-industries/xiv-data/refs/heads/main/exd/tc/{name}.csv",
+    "tc": "https://raw.githubusercontent.com/thewakingsands/ffxiv-datamining-tc/refs/heads/main/{name}.csv",
 }
 LANG = tuple(BASE_URL.keys())
 
